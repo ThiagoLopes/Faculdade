35ae23d4d4050d6f78ce18f3a74305ddb063deed
ThiagoLopes <rocketparadise@hotmail.com>
2016-04-12 17:24:45 -0300

    trabalho de redes

---
+++ Trabalho Redes de computadores/README.md
@@ -0,0 +1,11 @@
+##Trabalho de redes de computadores. :school:
+
+#Funcionamento :passport_control:
+
+```
+Cliente udp --->Proxy udp/tcp --->	Servidor tcp
+										|
+										|
+										|
+Cliente udp<---	Proxy tcp/udp	<--------
+```
---
+++ Trabalho Redes de computadores/tcp-servidor/main.cpp
@@ -0,0 +1,170 @@
+/*
+
+ Servidor TCP
+
+ */
+#define LINUX
+
+#ifdef WINDOWS
+#include <winsock.h>
+#include <windows.h>
+#endif
+
+#ifdef LINUX
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#define closesocket(n) close(n)
+#define SOCKET socklen_t
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+
+#define DEFAULT_PORT 64321
+#define IPADDRESS "127.0.0.1"
+#define MAX_SIZE 1024
+
+using namespace std;
+
+void strupr(char str[])
+{
+    int indx = 0;
+    for (; indx < 2; indx++)
+
+    {
+        if (str[indx] <= 122 && str[indx] >= 97)
+            str[indx] = (char)(((int)str[indx]) - 32);
+    }
+}
+
+
+
+
+int main(int argc, char **argv)
+{
+    char Buffer[MAX_SIZE];
+    int retval;
+#ifdef WINDOWS
+    int fromlen;
+#else
+    socklen_t fromlen;
+#endif
+
+    struct sockaddr_in local, from;
+    SOCKET listen_socket, msgsock;
+
+// cliente
+
+#ifdef WINDOWS
+    WSADATA wsaData;
+
+    if ((retval = WSAStartup(0x202, &wsaData)) != 0)
+    {
+        cout << "Server: WSAStartup() failed with error: " << retval << endl;
+        WSACleanup();
+        return -1;
+    }
+    else
+        cout << "Server: WSAStartup() is OK." << endl;
+#endif
+
+    local.sin_family = AF_INET;
+    local.sin_addr.s_addr = inet_addr(IPADDRESS);
+    local.sin_port = htons(DEFAULT_PORT);
+
+    listen_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (listen_socket < 0)
+    {
+        cout << "Server: socket() failed with error: " << listen_socket << endl;
+        exit(-1);
+    }
+    else
+        cout << "Server: socket() is OK." << endl;
+
+    if (bind(listen_socket, (struct sockaddr*) &local, sizeof (local)) < 0)
+    {
+        cout << "Server: bind() failed with error: " << endl;
+        exit(-2);
+    }
+
+    cout << "Server: bind() is OK.";
+
+    if (listen(listen_socket, 5) < 0)
+    {
+        cout << "Server: listen() failed with error: " << endl;
+        exit(-3);
+    }
+    else
+        cout << "Server: listen() is OK" << endl;
+
+    cout << "Server: listening and waiting connection on port " << DEFAULT_PORT << endl;
+
+    while (1)
+    {
+        memset(Buffer,0x0,MAX_SIZE);
+
+        fromlen = sizeof (from);
+
+        msgsock = accept(listen_socket,
+                         (struct sockaddr*) &from,
+                         &fromlen);
+        if (msgsock < 0)
+        {
+            cout << "Server: accept() error: " << msgsock << endl;
+            exit(-4);
+        }
+        else
+            cout << "Server: accept() is OK" << endl;
+
+        cout << "Server: accepted connection from " << inet_ntoa(from.sin_addr);
+        cout << " Port: " << htons(from.sin_port) << endl;
+
+        retval = recv(msgsock, Buffer, sizeof (Buffer), 0);
+
+        if (retval < 0)
+        {
+            cout << "No bytes received" << endl;
+            closesocket(msgsock);
+            continue;
+        }
+        else
+            printf("Server: recv() is OK.\n");
+
+        if (retval == 0)
+        {
+            printf("Server: Client closed connection.\n");
+            closesocket(msgsock);
+            continue;
+        }
+
+        cout << "Server: Received " << retval << " bytes" << endl;
+        cout << "Data: " << Buffer << endl;
+
+        printf("Server: Echoing the same data back to client...\n");
+
+        strupr(Buffer);
+
+
+        retval = send(msgsock, Buffer, sizeof (Buffer), 0);
+
+
+        if (retval < 0)
+        {
+            cout << "Server: send() failed: error: " << endl;
+        }
+        else
+            cout << "Server: send() is OK." << endl;
+
+        closesocket(msgsock);
+    }
+
+    closesocket(listen_socket);
+
+    exit(0);
+}
